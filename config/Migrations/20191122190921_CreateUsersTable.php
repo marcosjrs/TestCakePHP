202de25e35c16aa05236333164f3d86e2558f535
Marcos JRS <marcosjrs80@gmail.com>
2019-11-22 20:42:42 +0100

    Creacion tabla users, mediante cake migrations migrate

--- config/Migrations/20191122190921_CreateUsersTable.php
+++ config/Migrations/20191122190921_CreateUsersTable.php
@@ -5,12 +5,24 @@ class CreateUsersTable extends AbstractMigration
 {
     /**
      * Change Method.
-     *
-     * More information on this method is available here:
+     * Ejecutar en consola: bin\cake migrations migrate
+     * Para hacer efectivos los cambios expuesto en este método.
+     * Para revertirla (cuando no importe los datos contenicos), ejecutar 
+     * en consola: bin\cake migracion rollback
      * http://docs.phinx.org/en/latest/migrations.html#the-change-method
      * @return void
      */
     public function change()
     {
+        // creación de la tabla users
+        $table = $this->table('users');
+        $table->addColumn('first_name','string',array('limit' => 100))
+            ->addColumn('email','string',array('limit' => 100))
+            ->addColumn('password','string')
+            ->addColumn('role','enum',array('values'=>'admin,user'))
+            ->addColumn('active','boolean')
+            ->addColumn('created','datetime')
+            ->addColumn('modified','datetime')
+            ->create();
     }
 }
