ccc29be8994e72ee0eab398352e3c29652853143
Marcos JRS <marcosjrs80@gmail.com>
2019-11-22 22:24:45 +0100

    Uso de Faker, para poblar tabla users. Tras esto: cake migrations migrate

--- config/Migrations/20191122210156_CreateAdminSeedMigration.php
+++ config/Migrations/20191122210156_CreateAdminSeedMigration.php
@@ -1,16 +1,34 @@
 <?php
+
+use Cake\Auth\DefaultPasswordHasher;
 use Migrations\AbstractMigration;
 
 class CreateAdminSeedMigration extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * More information on this method is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
-     * @return void
-     */
-    public function change()
-    {
+    public function up(){
+
+        //https://github.com/fzaninotto/Faker#populating-entities-using-an-orm-or-an-odm
+        //Poblamos la tabla Users con un registro, para hacer pruebas 
+        $faker = \Faker\Factory::create();
+        $populator = new Faker\ORM\CakePHP\Populator($faker);
+        $populator->addEntity('Users', 1, [
+            'first_name' => 'Marcos',
+            'last_name' => 'JRS',
+            'email' => 'marcosjrs@email.com',
+            'password' => function(){
+                $hasher = new DefaultPasswordHasher();
+                return $hasher->hash('secret');
+            },
+            'role' => 'admin',
+            'active' => 1,
+            // función anonima que usará los recursos de faker para devolver un datetime.
+            'created' => function() use($faker){
+                return $faker->dateTimeBetween($startDate = 'now', $endDate = 'now');
+            },
+            'modified' => function() use($faker){
+                return $faker->dateTimeBetween($startDate = 'now', $endDate = 'now');
+            }
+        ]);
+        $populator->execute();
     }
 }
